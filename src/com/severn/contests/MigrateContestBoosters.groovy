078d767894e57339ad165cd8c823fb55e1506afb
Sergey Shcherbovich <sergey.shcherbovich@synesis.local>
2017-04-17 15:16:25 +0300

    Migrate ContestBoosters from one entity to another

---
+++ src/com/severn/contests/MigrateContestBoosters.groovy
@@ -0,0 +1,86 @@
+import java.util.logging.*;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.context.ApplicationContext;
+
+import javax.mail.*;
+import javax.mail.internet.*
+
+import com.google.appengine.api.datastore.DatastoreService
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory
+import com.google.apphosting.api.ApiProxy;
+import com.severn.common.bigquery.BigQueryServiceSupport;
+import com.severn.common.domain.User;
+import com.severn.common.utils.BigQueryUtil
+
+import com.severn.datastore.legacy.*
+
+DatastoreService ds = DatastoreServiceFactory.getDatastoreService()
+MemcacheService  mc = MemcacheServiceFactory.getMemcacheService()
+
+def entities = []
+def dsUtil = new DatastoreUtil('ContestBooster')
+
+dsUtil.shouldUpdate = { Entity entity ->
+    def cnt = entity.getProperty('count')
+    if (cnt && (cnt > 1)) {
+        entities << entity
+    }
+    false
+}
+
+dsUtil.updateEntity = { Entity entity ->
+    null
+}
+
+def infos = dsUtil.execute()
+
+def newKeys = entities.collect { Entity it -> KeyFactory.createKey('ContestBoosterState', it.key.id) }
+def ents = ds.get(newKeys).values()
+def olds = entities.collectEntries { Entity it -> [(it.key.id) : it] }
+
+ents.each { Entity it ->
+    logger.fine("Old Entity : ${it}")
+    Entity oldEnt = olds[it.key.id]
+    def cntOld = oldEnt.getProperty('count')
+    def cntNew = it.getProperty('count')
+    it.setUnindexedProperty('count', cntOld + cntNew)
+    logger.fine("New Entity : ${it}")
+}
+
+def mcKeys = ents.collect { Entity it -> "ContestBooster_${it.key.id}".toString() }
+
+ds.put(ents)
+mc.deleteAll(mcKeys)
+
+def result = 'Ok ' + entities.size()
+//notifyEnded('sergey.shcherbovich@synesis.ru', result)
+result
+
+void notifyEnded(def reciever, def message) {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+    
+    try {
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("${ApiProxy.getCurrentEnvironment().getAppId().replace('s~', '')}@appspot.gserviceaccount.com", "Script executor module"));
+        msg.addRecipient(Message.RecipientType.TO,
+         new InternetAddress(reciever, "Dear DEV"));
+        msg.setSubject("Script was executed successfully at ${new Date()}");
+        msg.setText(message);
+        Transport.send(msg);
+    
+    } catch (AddressException e) {
+        Logger.getLogger('com.severn').log(Level.WARNING, "Got", e)
+    } catch (MessagingException e) {
+        Logger.getLogger('com.severn').log(Level.WARNING, "Got", e)
+    }
+}
