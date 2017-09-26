e2899ba756eb77bb2eb01b7a443edb694dbed3ca
Sergey Shcherbovich <sergey.shcherbovich@synesis.local>
2017-09-26 11:47:48 +0300

    CheckDuplicateExpiredSubscriptions MoveExpiredSubscriptionsIds

---
+++ src/com/severn/payments/MoveExpiredSubscriptionsIds.groovy
@@ -0,0 +1,76 @@
+import java.util.logging.*;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.springframework.context.ApplicationContext;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import com.google.appengine.api.datastore.DatastoreService
+import com.google.appengine.api.datastore.DatastoreServiceFactory
+import com.google.appengine.api.datastore.Entity
+import com.google.appengine.api.datastore.KeyFactory
+import com.google.appengine.api.datastore.Query
+import com.google.appengine.api.datastore.Text
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.gson.Gson
+import com.severn.common.bigquery.BigQueryServiceSupport;
+import com.severn.common.domain.User;
+import com.severn.common.utils.BigQueryUtil;
+import com.severn.datastore.legacy.DatastoreUtil;
+import com.severn.payments.domain.SubscriptionInfo
+
+ApplicationContext ctx = binding.variables.get('applicationContext')
+BigQueryServiceSupport bigQueryClient = ctx.getBean('bigQueryServiceSupport')
+DateTime dt = DateTime.now(DateTimeZone.UTC)
+DateTimeFormatter f = DateTimeFormat.forPattern('yyyy-MM-dd').withZoneUTC()
+DatastoreService ds = DatastoreServiceFactory.getDatastoreService()
+Gson g = new Gson()
+
+Query q = new Query('ExpiredSubscription')
+
+def i = ds.prepare(q).asIterable()
+
+def mapping = [:] as HashMap
+def entitiesToDelete = []
+def badsCnt = 0
+
+i.each { Entity e ->
+    String json = ((Text)e.getProperty('subscription'))?.value
+    SubscriptionInfo si = g.fromJson(json, SubscriptionInfo.class)
+    String id = si.getRecieptId()
+    
+    Entity newE = new Entity(KeyFactory.createKey('ExpiredSubscription', id))
+    newE.setUnindexedProperty('subscription', new Text(json))
+    
+    ds.put(newE)
+    ds.delete(e.key)
+}
+
+def result = 'Ok duplicates ' + badsCnt + ', oks ' + mapping.size
+notifyEnded('sergey.shcherbovich@synesis.ru', bigQueryClient.getServiceAccountId(), result)
+result
+
+void notifyEnded(def reciever, def sender, def message) {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+    
+    try {
+        MimeMessage msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress(sender, "Script executor module"));
+        msg.addRecipient(Message.RecipientType.TO,
+         new InternetAddress(reciever, "Dear DEV"));
+        msg.setSubject("Script was executed successfully at ${new Date()}");
+        msg.setText(message);
+        Transport.send(msg);
+    
+    } catch (AddressException e) {
+        Logger.getLogger('com.severn').log(Level.WARNING, "Got", e)
+    } catch (MessagingException e) {
+        Logger.getLogger('com.severn').log(Level.WARNING, "Got", e)
+    }
+}
